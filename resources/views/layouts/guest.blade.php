793c2f94aca165e15443a2eb1e0258a9fd8962ca
Edo <pedoprimasaragi@gmail.com>
2025-12-19 18:25:32 +0700

    Fix CSS missing styles and use Vite

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -23,7 +23,7 @@
     
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css">
     
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}?v={{ time() }}">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     
     @stack('styles')
     
